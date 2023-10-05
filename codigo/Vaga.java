9fbefef32c164ef87b391c11117980bf8216ef40
Lucas Jácome <104047219+lucasjacome@users.noreply.github.com>
2023-10-04 22:04:55 -0300

    Atualização do Vaga.java
    Primeiro commit do Projeto 3.

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -2,23 +2,38 @@
 
 public class Vaga {
 
-	private String id;
-	private boolean disponivel;
+    private String id;
+    private boolean disponivel;
 
-	public Vaga(int fila, int numero) {
-		
-	}
+    public Vaga(int fila, int numero) {
+        
+    }
 
-	public boolean estacionar() {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public boolean estacionar() {
         return false;
     }
 
-	public boolean sair() {
+    public boolean sair() {
         return false;
     }
 
-	public boolean disponivel() {
+    public boolean disponivel() {
         return false;
     }
-
 }
