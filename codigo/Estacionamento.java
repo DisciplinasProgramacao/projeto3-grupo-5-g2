fab111620e9438825fddee25524d38a15529dad9
Matheus Miranda Cançado <91295666+MatheusCancado@users.noreply.github.com>
2023-10-18 21:56:48 -0300

    Update Estacionamento.java

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -3,19 +3,41 @@
 public class Estacionamento {
 
 	private String nome;
-	private Cliente[] id;
+	private HashMap<>[] clientes;
 	private Vaga[] vagas;
 	private int quantFileiras;
 	private int vagasPorFileira;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
+
+	this.nome = nome;
+        this.quantFileiras = quantFileiras;
+        this.vagasPorFileira = vagasPorFila;
+        this.clientes = new HashMap<>();
+        this.veiculosEstacionados = new HashMap<>();
+        this.vagas = new Vaga[quantFileiras * vagasPorFileira];
+        inicializarVagas()
+	 }
+
+    private void inicializarVagas() {
+        for (int i = 0; i < vagas.length; i++) {
+            vagas[i] = new Vaga(i + 1);
+        }
 		
 	}
 
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
-	}
+        Cliente clientes = clientes.get(idCli);
+        if (cliente != null) {
+            veiculosEstacionados.put(veiculo.getPlaca(), veiculo);
+            cliente.adicionarVeiculo(veiculo);
+            estacionar(veiculo.getPlaca());
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+	
 	public static void getVeiculo(){
 		return veiculo;
 	}
