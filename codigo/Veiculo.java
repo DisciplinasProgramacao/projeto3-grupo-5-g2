fc6f8816eba5bac885726639dc757adebe63dca1
tuzxo <104038065+tuzxo@users.noreply.github.com>
2023-10-25 18:24:44 -0300

    Update Veiculo.java
    Sem documentação pois ainda está sendo feita.

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,32 +1,75 @@
-
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Veiculo {
-
 	private String placa;
-	private UsoDeVaga[] usos;
+	private List<UsoDeVaga> usos;
+	private LocalDateTime horaEntrada;
+	private Cliente cliente;
 
 	public Veiculo(String placa) {
-		
+		this.placa = placa;
+		this.usos = new ArrayList<>();
 	}
 
-	public void estacionar(Vaga vaga) {
-		
+	public Cliente getCliente() {
+		return cliente;
 	}
 
-	public double sair() {
-        return 0;
-    }
+	public String getPlaca() {
+		return placa;
+	}
+
+	public LocalDateTime getHoraEntrada() {
+		return horaEntrada; // Obtenha a hora de entrada do veículo
+	}
+
+	public void estacionar(Vaga vaga, LocalDateTime entrada) {
+		UsoDeVaga uso = new UsoDeVaga(vaga, this, entrada);
+		usos.add(uso);
+		vaga.estacionar(this, uso);
+	}
+
+	public double sair(Vaga vaga, LocalDateTime saida) {
+		UsoDeVaga uso = encontrarUso(vaga);
+		if (uso != null) {
+			double valorPago = uso.sair(saida);
+			usos.remove(uso);
+			return valorPago;
+		}
+		return 0.0; // Veículo não estava estacionado nesta vaga.
+	}
+
+	private UsoDeVaga encontrarUso(Vaga vaga) {
+		for (UsoDeVaga uso : usos) {
+			if (Objects.equals(uso.getVaga(), vaga)) {
+				return uso;
+			}
+		}
+		return null;
+	}
 
 	public double totalArrecadado() {
-        return 0;
-    }
+		double total = 0.0;
+		for (UsoDeVaga uso : usos) {
+			total += uso.getValorPago();
+		}
+		return total;
+	}
 
 	public double arrecadadoNoMes(int mes) {
-        return 0;
-    }
+		double total = 0.0;
+		for (UsoDeVaga uso : usos) {
+			if (uso.getEntrada().getMonthValue() == mes) {
+				total += uso.getValorPago();
+			}
+		}
+		return total;
+	}
 
 	public int totalDeUsos() {
-        return 0;
-    }
-
+		return usos.size();
+	}
 }
