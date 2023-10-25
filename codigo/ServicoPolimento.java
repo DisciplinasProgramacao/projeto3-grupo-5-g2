bff2727215e282d73f7ce5de38b85fd2f320b9ac
tuzxo <104038065+tuzxo@users.noreply.github.com>
2023-10-25 14:24:42 -0300

    Add files via upload

---
+++ codigo/ServicoPolimento.java
@@ -0,0 +1,11 @@
+/**
+ * Classe que representa o serviço de Polimento (inclui Lavagem).
+ */
+public class ServicoPolimento extends Servicos {
+    /**
+     * Construtor da classe ServicoPolimento.
+     */
+    public ServicoPolimento() {
+        super("Polimento", 45.0, 2); // Valor fixo, tempo mínimo de 2 horas
+    }
+}
