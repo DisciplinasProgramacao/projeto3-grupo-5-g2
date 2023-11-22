ce98c6de344da0675ea8e7620c128001320cfae2
Lucas Jácome <104047219+lucasjacome@users.noreply.github.com>
2023-11-22 18:46:50 -0300

    Update TesteVeiculo.java

--- codigo/TesteVeiculo.java
+++ codigo/TesteVeiculo.java
@@ -1,16 +1,42 @@
+import static org.junit.Assert.assertEquals;
 
-public class TesteVeiculo {
-  
-    public static void main(String[] args) {
-      
-        Veiculo veiculo = new Veiculo("ABC123");
-        Vaga vaga = new Vaga('A', 1);
-        
-        veiculo.estacionar(vaga); // Veículo com placa ABC123 estacionado na vaga A1
-        veiculo.sair(); // Veículo com placa ABC123 saiu da vaga. Valor pago: R$10.0
-        
-        System.out.println("Total arrecadado: R$" + veiculo.totalArrecadado());
-        System.out.println("Arrecadado no mês 10: R$" + veiculo.arrecadadoNoMes(10));
-        System.out.println("Total de usos: " + veiculo.totalDeUsos());
+import org.junit.Test;
+
+import entities.Vaga;
+
+public class TestVaga {
+
+    @Test
+    public void testEstacionarVagaNaoDisponivel() {
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertEquals(false, vaga.estacionar());
+    }
+
+    @Test
+    public void testSairVagaDisponivel() {
+        Vaga vaga = new Vaga(1, 1);
+        assertEquals(true, vaga.sair());
+    }
+
+    @Test
+    public void testSairVagaNaoDisponivel() {
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertEquals(true, vaga.sair());
+    }
+
+    @Test
+    public void testDisponivelVagaDisponivel() {
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = true;
+        assertEquals(true, vaga.disponivel());
+    }
+
+    @Test
+    public void testDisponivelVagaNaoDisponivel() {
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertEquals(false, vaga.disponivel());
     }
 }
