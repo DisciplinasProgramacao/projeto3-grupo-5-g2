8beaf8b38fba76cc66476576addeda653f64050e
Lucas Jácome <104047219+lucasjacome@users.noreply.github.com>
2023-11-22 19:11:30 -0300

    Update Vaga.java

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -1,31 +1,31 @@
-
-
 public class Vaga {
 
     private String id;
     private boolean disponivel;
+    private String filas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+     /**
+     * Construtor da classe Vaga.
+     *
+     * @param fila   A letra da fila da vaga.
+     * @param numero O número da vaga.
+     */
     public Vaga(char fila, int numero) {
-        this.id = "Vaga " + fila + numero;
-        this.disponivel = true;
+        String vagaFinal;
+        String letraVaga = "";
+        letraVaga += filas.charAt(fila);
+        vagaFinal = letraVaga + numero;
+
+        // Define a disponibilidade da vaga com base no valor da vagaFinal
+        if (vagaFinal != null)
+            disponivel = false;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public boolean isDisponivel() {
-        return disponivel;
-    }
-
-    public void setDisponivel(boolean disponivel) {
-        this.disponivel = disponivel;
-    }
-
+    /**
+     * Estaciona um veículo na vaga, se a vaga estiver disponível.
+     *
+     * @return true se o veículo foi estacionado com sucesso, false se a vaga estiver ocupada.
+     */
     public boolean estacionar() {
         if (disponivel) {
             disponivel = false;
@@ -37,6 +37,11 @@ public class Vaga {
         }
     }
 
+    /**
+     * Libera a vaga, marcando-a como disponível.
+     *
+     * @return true se a vaga foi liberada com sucesso, false se a vaga já estiver vazia.
+     */
     public boolean sair() {
         if (!disponivel) {
             disponivel = true;
@@ -48,4 +53,44 @@ public class Vaga {
         }
     }
 
+    /**
+     * Obtém o identificador único da vaga.
+     *
+     * @return O identificador único da vaga.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Define o identificador único da vaga.
+     *
+     * @param id O novo identificador único da vaga.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    /**
+     * Verifica se a vaga está disponível.
+     *
+     * @return true se a vaga estiver disponível, false se estiver ocupada.
+     */
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    
+    /**
+     * Define a disponibilidade da vaga.
+     *
+     * @param disponivel O novo estado de disponibilidade da vaga (true para disponível, false para ocupada).
+     */
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
