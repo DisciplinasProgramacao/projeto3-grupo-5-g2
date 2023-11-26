36b05f96694425cae9828740c8c167b98a0ef738
Lucas Jácome <104047219+lucasjacome@users.noreply.github.com>
2023-11-25 23:46:59 -0300

    Update SistemaEstacionamento.java (Main)

--- codigo/SistemaEstacionamento.java
+++ codigo/SistemaEstacionamento.java
@@ -26,6 +26,34 @@ public class SistemaEstacionamento {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            menu();
+            int opcao = scanner.nextInt();
+
+
+            switch (opcao) {
+                 case 1:
+                    cadastrarCliente();
+                    break;
+                case 2:
+                    cadastrarVeiculo();
+                    break;
+                case 3:
+                    estacionarVeiculo();
+                    break;
+                case 4:
+                    contratarServiçoAdd();
+                    break;
+                case 5:
+                    sairVaga();
+                    break;
+                case 6:
+                    encerrarSistema();
+                    break;
+            }
+
+        }
+
+        private void menu() {
             System.out.println("Bem-vindo ao Sistema de Estacionamento da Xulambs Parking!");
             System.out.println("Escolha uma opção:");
             System.out.println("1. Cadastrar Cliente");
@@ -35,65 +63,60 @@ public class SistemaEstacionamento {
             System.out.println("5. Sair do estacionamento");
             System.out.println("6. Encerrar o sistema");
 
-            int opcao = scanner.nextInt();
+        }
 
-            switch (opcao) {
+        private void cadastrarCliente() {
+            System.out.println("------ Cadastro de Cliente ------");
+            System.out.print("Nome do cliente: ");
+            String nome = scanner.nextLine();
+            System.out.print("Id do cliente: ");
+            String id = scanner.nextLine();
 
-                case 1:
-                    // Opção para cadastrar cliente
-                    System.out.println("------ Cadastro de Cliente ------");
-                    System.out.print("Nome do cliente: ");
-                    String nome = scanner.nextLine();
-                    System.out.print("ID do cliente: ");
-                    String id = scanner.nextLine();
+            System.out.println("Selecione o tipo de cliente:");
+            for (eCliente tipo : eCliente.values()) {
+                System.out.println((tipo.ordinal() + 1) + ". " + tipo.getNome());
+            }
+            int escolhaTipo = scanner.nextInt();
+            scanner.nextLine();
+            eCliente tipoCliente = null;
+            if (escolhaTipo > 0 && escolhaTipo <= eCliente.values().length) {
+                tipoCliente = eCliente.values()[escolhaTipo - 1];
+            }
 
-                    System.out.println("Selecione o tipo de cliente:");
-                    for (eCliente tipo : eCliente.values()) {
-                        System.out.println((tipo.ordinal() + 1) + ". " + tipo.getNome());
-                    }
-                    int escolhaTipo = scanner.nextInt();
-                    scanner.nextLine();
-                    eCliente tipoCliente = null;
-                    if (escolhaTipo > 0 && escolhaTipo <= eCliente.values().length) {
-                        tipoCliente = eCliente.values()[escolhaTipo - 1];
-                    }
+            if (tipoCliente == eCliente.TURNO) {
+                System.out.println("Selecione o turno do cliente: ");
+                for (eTurnos turno : eTurnos.values()) {
+                    System.out.println((turno.ordinal() + 1) + ". " + turno.getNome());
+                }
+                int escolhaTurno = scanner.nextInt();
+                scanner.nextLine();
+                eTurnos turnoCliente = null;
+                if (escolhaTurno > 0 && escolhaTurno <= eTurnos.values().length) {
+                    turnoCliente = eTurnos.values()[escolhaTurno - 1];
+                } else {
+                    System.out.println("Turno escolhido invalido! O cliente será registrado sem turno.");
+                }
 
-                    if (tipoCliente == eCliente.TURNO) {
-                        System.out.println("Selecione o turno do cliente:");
-                        for (eTurnos turno : eTurnos.values()) {
-                            System.out.println((turno.ordinal() + 1) + ". " + turno.getNome());
-                        }
-                        int escolhaTurno = scanner.nextInt();
-                        scanner.nextLine();
-                        eTurnos turnoCliente = null;
-                        if (escolhaTurno > 0 && escolhaTurno <= eTurnos.values().length) {
-                            turnoCliente = eTurnos.values()[escolhaTurno - 1];
-                        } else {
-                            System.out.println("Escolha inválida para o turno. O cliente será registrado sem turno.");
-                        }
+                Cliente cliente = new Cliente(nome, id, tipoCliente, turnoCliente);
+                estacionamento.addCliente(cliente);
+            } else {
+                Cliente cliente = new Cliente(nome, id, tipoCliente);
+                estacionamento.addCliente(cliente);
+            }
+        }
 
-                        Cliente cliente = new Cliente(nome, id, tipoCliente, turnoCliente);
-                        estacionamento.addCliente(cliente);
-                    } else {
-                        Cliente cliente = new Cliente(nome, id, tipoCliente);
-                        estacionamento.addCliente(cliente);
-                    }
-                    break;
+        private void cadastrarVeiculo() {
+            System.out.println("------ Cadastro de Veículo ------");
+            System.out.print("Placa do Veiculo: ");
+            String placa = scanner.nextLine();
+            Veiculo veiculo = new Veiculo(placa);
+            System.out.print("Id do cliente: ");
+            String id = scanner.nextLine();
+            estacionamento.addVeiculo(veiculo, id);
+        }
 
-                case 2:
-                    // Opção para cadastrar veículo
-                    System.out.println("------ Cadastro de Veículo ------");
-                    System.out.print("Placa do Veiculo: ");
-                    String placa = scanner.nextLine();
-                    Veiculo veiculo = new Veiculo(placa);
-                    System.out.print("ID DO CLIENTE: ");
-                    String id = scanner.nextLine();
-                    estacionamento.addVeiculo(veiculo, id);
-                    break;
-
-                case 3:
-                    // Opção para estacionar veículo
-                    System.out.println("Informe a placa do veículo:");
+        private void estacionarVeiculo() {
+            System.out.println("Informe a placa do veículo:");
                     String placa = scanner.next();
                     System.out.println("Informe a vaga (ID da vaga) onde deseja estacionar:");
                     String idVaga = scanner.next();
@@ -113,12 +136,12 @@ public class SistemaEstacionamento {
                         }
                     } else {
                         System.out.println("Veículo ou vaga não encontrados. Verifique os dados e tente novamente.");
-                    }
-                    break;
+                    }              
 
-                case 4:
-                    // Opção para contratar serviço adicional
-                    System.out.println("Informe a placa do veículo:");
+        }
+
+        private void contratarServiçoAdd() {
+            System.out.println("Informe a placa do veículo:");
                     placa = scanner.next();
 
                     clienteEncontrado = cliente.encontrarClientePorPlaca(placa);
@@ -157,12 +180,12 @@ public class SistemaEstacionamento {
                         }
                     } else {
                         System.out.println("Veículo não encontrado. Verifique os dados e tente novamente.");
-                    }
-                    break;
+                    }          
 
-                case 5:
-                    // Opção para sair da vaga
-                    System.out.println("Informe a placa do veículo que deseja sair:");
+        }
+
+        private void sairVaga() {
+            System.out.println("Informe a placa do veículo que deseja sair:");
                     String placaSaida = scanner.next();
 
                     clienteEncontrado = estacionamento.encontrarClientePorPlaca(placaSaida);
@@ -195,14 +218,14 @@ public class SistemaEstacionamento {
                     } else {
                         System.out.println("Veículo não encontrado. Verifique os dados e tente novamente.");
                     }
-                    break;
 
-                case 6:
-                    System.out.println("Encerrando o sistema de estacionamento.");
+        }
+
+        private void encerrarSistema() {
+            System.out.println("Encerrando o sistema de estacionamento.");
                     scanner.close(); // Fecha o scanner
                     System.exit(0); // Encerra o programa
-                    break;
-            }
         }
+
     }
 }
